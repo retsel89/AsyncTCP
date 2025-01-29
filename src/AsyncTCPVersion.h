afc39c18eee65a7e1d8ef809feb93764918b3de7
me-no-dev <hristo@espressif.com>
2025-01-29 12:08:17 +0200

    Update library version to 3.3.3

--- src/AsyncTCPVersion.h
+++ src/AsyncTCPVersion.h
@@ -12,7 +12,7 @@ extern "C" {
 /** Minor version number (x.X.x) */
 #define ASYNCTCP_VERSION_MINOR 3
 /** Patch version number (x.x.X) */
-#define ASYNCTCP_VERSION_PATCH 2
+#define ASYNCTCP_VERSION_PATCH 3
 
 /**
  * Macro to convert version number into an integer
