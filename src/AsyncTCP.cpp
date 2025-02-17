d98d1e235a8d3d726255d4410f2383fba25b68c6
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-02-17 14:46:04 +0100

    Directly reset callbacks without going through lwip functions (fix #30)

--- src/AsyncTCP.cpp
+++ src/AsyncTCP.cpp
@@ -448,8 +448,22 @@ static int8_t _tcp_sent(void *arg, struct tcp_pcb *pcb, uint16_t len) {
   return ERR_OK;
 }
 
-static void _tcp_error(void *arg, int8_t err) {
+void AsyncClient::_tcp_error(void *arg, int8_t err) {
   // ets_printf("+E: 0x%08x\n", arg);
+  AsyncClient *client = reinterpret_cast<AsyncClient *>(arg);
+  if (client && client->_pcb) {
+    tcp_arg(client->_pcb, NULL);
+    if (client->_pcb->state == LISTEN) {
+      tcp_sent(client->_pcb, NULL);
+      tcp_recv(client->_pcb, NULL);
+      tcp_err(client->_pcb, NULL);
+      tcp_poll(client->_pcb, NULL, 0);
+    }
+    client->_pcb = nullptr;
+    client->_free_closed_slot();
+  }
+
+  // enqueue event to be processed in the async task for the user callback
   lwip_tcp_event_packet_t *e = (lwip_tcp_event_packet_t *)malloc(sizeof(lwip_tcp_event_packet_t));
   if (!e) {
     log_e("Failed to allocate event packet");
@@ -459,7 +473,7 @@ static void _tcp_error(void *arg, int8_t err) {
   e->arg = arg;
   e->error.err = err;
   if (!_send_async_event(&e)) {
-    free((void *)(e));
+    ::free((void *)(e));
   }
 }
 
@@ -1046,19 +1060,6 @@ int8_t AsyncClient::_connected(tcp_pcb *pcb, int8_t err) {
 }
 
 void AsyncClient::_error(int8_t err) {
-  if (_pcb) {
-    TCP_MUTEX_LOCK();
-    tcp_arg(_pcb, NULL);
-    if (_pcb->state == LISTEN) {
-      tcp_sent(_pcb, NULL);
-      tcp_recv(_pcb, NULL);
-      tcp_err(_pcb, NULL);
-      tcp_poll(_pcb, NULL, 0);
-    }
-    TCP_MUTEX_UNLOCK();
-    _free_closed_slot();
-    _pcb = NULL;
-  }
   if (_error_cb) {
     _error_cb(_error_cb_arg, this, err);
   }
