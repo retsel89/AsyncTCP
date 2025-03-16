1088a8d04714dab616ad590906ae0605dc3a10c6
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-03-16 12:04:57 +0100

    v3.3.7

--- src/AsyncTCPVersion.h
+++ src/AsyncTCPVersion.h
@@ -12,7 +12,7 @@ extern "C" {
 /** Minor version number (x.X.x) */
 #define ASYNCTCP_VERSION_MINOR 3
 /** Patch version number (x.x.X) */
-#define ASYNCTCP_VERSION_PATCH 6
+#define ASYNCTCP_VERSION_PATCH 7
 
 /**
  * Macro to convert version number into an integer
